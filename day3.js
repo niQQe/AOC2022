b6816266077f85be3605ee4840d69c3f6241cedf
unknown <nickewideving@gmail.com>
2022-12-04 12:46:16 +0100

    day 3 complete

--- day3.js
+++ day3.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
-
 const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
 const input = fs.readFileSync('day3-input.txt', 'utf8').split`\n`
 
 /** Part 1 */
@@ -15,5 +13,21 @@ const sumPart1 = input.map(r => {
 		}
 	}
 }, 0)
-
 console.log(sumPart1);
+
+/** Part 2 */
+const sumPart2 = input.reduce((groups, _, index, array) => {
+	groups.push(array.slice(index > 0 ? index * 3 : index, index > 0 ? index * 3 + 3 : index + 3))
+	return groups
+}, []).filter(v => v.length).reduce((score, rucksacks) => {
+	const [first, second, third] = rucksacks
+	for (const item of first.split``) {
+		if (second.includes(item) && third.includes(item)) {
+			score += alphabet.indexOf(item) + 1
+			break
+		}
+	}
+	return score
+}, 0)
+console.log(sumPart2);
+
