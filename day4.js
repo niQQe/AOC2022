a346dd5dfef7492e47810fe0acfc05c7f31e0c54
unknown <nickewideving@gmail.com>
2022-12-05 20:23:19 +0100

    day 4 compelte

--- day4.js
+++ day4.js
@@ -2,18 +2,27 @@ const fs = require('fs');
 
 const input = fs.readFileSync('day4-input.txt', 'utf8').split`\n`
 
-/** Part1 */
-const resultPart = input.reduce((fullyContains, pairs) => {
+/** Part 1 */
+const resultPart1 = input.reduce((total, pairs) => {
 	const [first, second] = pairs.split`,`
 	const [firstStart, firstEnd] = first.split`-`.map(v => +v)
 	const [secondStart, secondEnd] = second.split`-`.map(v => +v)
-	if ((firstStart <= secondStart && firstEnd >= secondEnd) || (secondStart <= firstStart && secondEnd >= firstEnd)) {
-		fullyContains++
-	}
-	return fullyContains
+	const isFullyOverLapping = (firstStart <= secondStart && firstEnd >= secondEnd) || (secondStart <= firstStart && secondEnd >= firstEnd)
+	if (isFullyOverLapping) total++
+	return total
 }, 0)
+console.log(resultPart1);
 
-console.log(resultPart);
+/** Part 2 */
+const resultPart2 = input.reduce((total, pairs) => {
+	const [first, second] = pairs.split`,`
+	const [firstStart, firstEnd] = first.split`-`.map(v => +v)
+	const [secondStart, secondEnd] = second.split`-`.map(v => +v)
+	const notOverLapping = firstStart > secondEnd && firstEnd > secondStart || firstStart < secondEnd && firstEnd < secondStart
+	if (notOverLapping) total--;
+	return total
+}, input.length)
+console.log(resultPart2);
 
 
 
