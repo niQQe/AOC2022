c5788d0a7dc8293cd992e521bab9649b7b39004f
unknown <nickewideving@gmail.com>
2022-12-18 20:23:55 +0100

    day 9

--- day9.js
+++ day9.js
@@ -6,24 +6,23 @@ let tailPosition = [0, 0]
 
 const isAdjacent = (headY, headX) => {
 	const [tailY, tailX] = tailPosition,
-		  tail = `${tailY},${tailX}`,
-		  right = `${headY},${headX + 1}` === tail,
-		  rightDown = `${headY + 1},${headX + 1}` === tail,
-		  rightUp = `${headY - 1},${headX + 1}` === tail,
-		  left = `${headY},${headX - 1}` === tail,
-		  leftUp = `${headY - 1},${headX - 1}` === tail,
-		  leftDown = `${headY + 1},${headX - 1}` === tail,
-		  down = `${headY + 1},${headX}` === tail,
-		  up = `${headY - 1},${headX}` === tail,
-		  overlapping = `${headY},${headX}` === tail
+		  right = `${headY},${headX + 1}`,
+		  rightDown = `${headY + 1},${headX + 1}`,
+		  rightUp = `${headY - 1},${headX + 1}`,
+		  left = `${headY},${headX - 1}`,
+		  leftUp = `${headY - 1},${headX - 1}`,
+		  leftDown = `${headY + 1},${headX - 1}`,
+		  down = `${headY + 1},${headX}`,
+		  up = `${headY - 1},${headX}`,
+		  overlapping = `${headY},${headX}`
 
-	return [right, left, down, up, rightDown, rightUp, leftDown, leftUp, overlapping].some(r => r)
+	return [right, left, down, up, rightDown, rightUp, leftDown, leftUp, overlapping].map(v => v === `${tailY},${tailX}`).some(r => r)
 };
 
 resultPart1 = input.reduce((result, move) => {
 	const [dir, steps] = move,
 		  [hy, hx] = headPosition
-		  
+
 	switch (dir) {
 		case 'R': {
 			for (let step = 0; step < steps; step++) {
