5b292f75113a0f6af8d7968f96a0422285c29e0d
unknown <nickewideving@gmail.com>
2022-12-18 20:07:08 +0100

    day 9

--- day9.js
+++ day9.js
@@ -4,19 +4,19 @@ const input = fs.readFileSync('day9-input.txt', 'utf-8').split`\n`.map(v => v.sp
 let headPosition = [0, 0]
 let tailPosition = [0, 0]
 
-const isNotAdjacent = (headY, headX) => {
+const isAdjacent = (headY, headX) => {
 	const [tailY, tailX] = tailPosition
 	const tail = `${tailY},${tailX}`
-	const right = `${headY},${headX + 1}` !== tail
-	const rightDown = `${headY + 1},${headX + 1}` !== tail
-	const rightUp = `${headY - 1},${headX + 1}` !== tail
-	const left = `${headY},${headX - 1}` !== tail
-	const leftUp = `${headY - 1},${headX - 1}` !== tail
-	const leftDown = `${headY + 1},${headX - 1}` !== tail
-	const down = `${headY + 1},${headX}` !== tail
-	const up = `${headY - 1},${headX}` !== tail
-	const overLapping = `${headY},${headX}` !== tail
-	return [right, left, down, up, rightDown, rightUp, leftDown, leftUp, overLapping].every(r => r)
+	const right = `${headY},${headX + 1}` === tail
+	const rightDown = `${headY + 1},${headX + 1}` === tail
+	const rightUp = `${headY - 1},${headX + 1}` === tail
+	const left = `${headY},${headX - 1}` === tail
+	const leftUp = `${headY - 1},${headX - 1}` === tail
+	const leftDown = `${headY + 1},${headX - 1}` === tail
+	const down = `${headY + 1},${headX}` === tail
+	const up = `${headY - 1},${headX}` === tail
+	const overLapping = `${headY},${headX}` === tail
+	return [right, left, down, up, rightDown, rightUp, leftDown, leftUp, overLapping].some(r => r)
 };
 
 const resultPart1 = input.reduce((result, move) => {
@@ -26,7 +26,7 @@ const resultPart1 = input.reduce((result, move) => {
 		case 'R': {
 			for (let step = 1; step <= steps; step++) {
 				headPosition[1]++
-				if (!isNotAdjacent(headPosition[0], headPosition[1])) continue
+				if (isAdjacent(headPosition[0], headPosition[1])) continue
 				tailPosition = [headPosition[0], headPosition[1] - 1]
 				result.add(`${hy},${headPosition[1] - 1}`)
 			}
@@ -35,7 +35,7 @@ const resultPart1 = input.reduce((result, move) => {
 		case 'U': {
 			for (let step = 1; step <= steps; step++) {
 				headPosition[0]--
-				if (!isNotAdjacent(headPosition[0], headPosition[1])) continue
+				if (isAdjacent(headPosition[0], headPosition[1])) continue
 				tailPosition = [headPosition[0] + 1, headPosition[1]]
 				result.add(`${headPosition[0] + 1},${hx}`)
 			}
@@ -44,7 +44,7 @@ const resultPart1 = input.reduce((result, move) => {
 		case 'D': {
 			for (let step = 1; step <= steps; step++) {
 				headPosition[0]++
-				if (!isNotAdjacent(headPosition[0], headPosition[1])) continue
+				if (isAdjacent(headPosition[0], headPosition[1])) continue
 				tailPosition = [headPosition[0] - 1, headPosition[1]]
 				result.add(`${headPosition[0] - 1},${hx}`)
 			}
@@ -53,7 +53,7 @@ const resultPart1 = input.reduce((result, move) => {
 		case 'L': {
 			for (let step = 1; step <= steps; step++) {
 				headPosition[1]--
-				if (!isNotAdjacent(headPosition[0], headPosition[1])) continue
+				if (isAdjacent(headPosition[0], headPosition[1])) continue
 				tailPosition = [headPosition[0], headPosition[1] + 1]
 				result.add(`${hy},${headPosition[1] + 1}`)
 			}
