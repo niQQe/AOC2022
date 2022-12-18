1c9e87f4ccc4fb16d3cf9d250cc8d6013bd030ba
unknown <nickewideving@gmail.com>
2022-12-18 19:55:26 +0100

    day 9

--- day9.js
+++ day9.js
@@ -36,11 +36,10 @@ const resultPart1 = input.reduce((result, move) => {
 		case 'U': {
 			for (let step = 1; step <= steps; step++) {
 				headPosition[0]--
-				if (isNotAdjacent(headPosition[0], headPosition[1])) {
-					tailPosition[1] = headPosition[1]
-					tailPosition[0] = headPosition[0] + 1
-					result.add(`${headPosition[0] + 1},${hx}`)
-				}
+				if (!isNotAdjacent(headPosition[0], headPosition[1])) continue
+				tailPosition[1] = headPosition[1]
+				tailPosition[0] = headPosition[0] + 1
+				result.add(`${headPosition[0] + 1},${hx}`)
 			}
 			break;
 		}
